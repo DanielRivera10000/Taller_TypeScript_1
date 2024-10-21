fc1880ccb0cb9228600f28d96b9c2d540b7c4ca8
DanielRivera10000 <de.riveraa1@uniandes.edu.co>
2024-10-20 19:49:41 -0500

    Primero avance

---
+++ Scripts/main.ts
@@ -0,0 +1,29 @@
+import {series} from './data.js';
+
+let seriesTable: HTMLElement = document.getElementById("seriesTableBody")!;
+
+
+function mostrarSeries(): void {
+    let tbodySeries = document.createElement("tbody");
+    series.forEach(serie => 
+    {
+        tbodySeries.innerHTML += `      
+        <tr>
+            <td>${serie.id}</td>
+            <td><a href="${serie.link}" target="_blank">${serie.name}</a></td>
+            <td>${serie.channel}</td>
+            <td>${serie.seasons}</td>
+        </tr>`;
+    });
+
+    seriesTable.appendChild(tbodySeries);
+}
+
+mostrarSeries();
+
+
+
+
+
+
+
